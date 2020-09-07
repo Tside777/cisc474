3e65be71cbcfb665ff90ab7ca23b9ab3068167db
Trevor Seidel <tside777@gmail.com>
2020-09-07 14:45:36 -0400

    Interactive JQuery

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-$(function(){
+$(function () {
     //this code runs after page is fully loaded
     console.log("test 1");
+
+
+    $("#nameform").on("submit", function (event) {
+        event.preventDefault();
+        $(".jumbotron").append($("#fname").val() + " " + $("#lname").val());
+        $(".jumbotron").removeClass('d-none');
+
+        console.log($("#fname").val());
+        console.log('should be showing jumbotron');
+        
+        return false;
+
     });
-
-
-$("#nameform").on("submit", function( event ) {
-    event.preventDefault();
-    console.log("Hello!");
-   /* event.preventDefault();
-    console.log("test");
-   // $(".jumbotron").prepend($("#fname".text + $("#lname".val()));
-    $(".jumbotron").show();
-    */
-
 });
